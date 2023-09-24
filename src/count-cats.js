aede0e3903bf6db306cd013552d04e7a5a29ef61
Oxana Savianok <oxanas8833@gmail.com>
2023-09-24 13:36:56 +0300

    feat: add a solution to count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,11 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+  return matrix.reduce((count, item) => count + item.filter(el => el === '^^').length, 0);
 }
-
 module.exports = {
   countCats
 };
