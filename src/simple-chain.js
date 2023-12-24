1d0fa0353012dd483204a3b54e9853355ea95fe1
Oxana Savianok <oxanas8833@gmail.com>
2023-12-24 14:06:47 +0300

    feat: add the beginning of the solution of the simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,22 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  currentChain: [],
+
   getLength() {
+    return this.currentChain.length;
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  },
+
+  addLink(value) {
+    this.currentChain.push(`${value}`);
+    return this.currentChain; 
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  },
+
+  removeLink(position) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
+
   reverseChain() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
+
   finishChain() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
